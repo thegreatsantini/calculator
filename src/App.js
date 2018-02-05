5f965411eab2ba761bdae690c6dcb25a8a0d8902
thegreatsantini <lss13@my.fsu.edu>
2018-02-05 13:27:09 -0800

    improve calculator functionality

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 // Task: keyboard (enter, backspace, operators)
 // Task: Material Design
-// Task: Equation Ui 
-// Task: GitHub
-
+// Task: Equation Ui
+// Task: 4 + 2 + 3 = 9
 class App extends Component {
   constructor() {
-    super()
+    super();
     this.state = {
-      number: ''
-    }
-    this.handleKeydown = this.handleKeydown.bind(this)
+      number: "",
+      result: ""
+    };
+    this.handleKeydown = this.handleKeydown.bind(this);
   }
 
   // handleChange(e) {
@@ -22,62 +22,68 @@ class App extends Component {
   // }
 
   handleNumber(number) {
-    const prevNumber = this.state.number;
-    this.setState({
-      number: Number(`${prevNumber}${number}`)
-    })
+    if (this.state.result !== "") {
+      this.handleClear();
+    }
+    this.setState(prevState => ({
+      number: Number(`${prevState.number}${number}`)
+    }));
   }
 
   handleOperator(operation) {
     this.setState({
       prevNumber: this.state.number,
       math: operation,
-      number: ''
-    })
-    
+      number: ""
+    });
   }
 
   handleClear() {
-    this.setState({
+    this.setState(() => ({
       math: undefined,
-      number: 0,
-      prevNumber: undefined
-    })
+      number: "",
+      prevNumber: undefined,
+      result: ""
+    }));
   }
 
   handleResult() {
     switch (this.state.math) {
-      case 'addition':
-        this.setState({number: this.state.prevNumber + this.state.number})
-        break
-      case 'subtraction':
-        this.setState({number: this.state.prevNumber - this.state.number})
-        break
-      case 'multiply':
-        this.setState({number: this.state.prevNumber * this.state.number})
-        break
-      case 'divide':
-        this.setState({number: this.state.prevNumber / this.state.number})
-        break
-      default: break
+      case "addition":
+        this.setState({ result: this.state.prevNumber + this.state.number });
+        break;
+      case "subtraction":
+        this.setState({ result: this.state.prevNumber - this.state.number });
+        break;
+      case "multiply":
+        this.setState({ result: this.state.prevNumber * this.state.number });
+        break;
+      case "divide":
+        this.setState({ result: this.state.prevNumber / this.state.number });
+        break;
+      default:
+        break;
     }
   }
 
   handleKeydown(e) {
-    console.log('e.key', e.key)
+    console.log("e.key", e.key);
+    e.preventDefault()
+    e.stopPropagation()
     if (Number.isInteger(Number(e.key))) {
-      this.handleNumber(e.key)
+      this.handleNumber(e.key);
     }
 
-    if (e.key === '-') {
-      this.handleSubtract()
+    if (e.key === "-") {
+      this.handleOperator('subtraction')
+    } else if ( e.key === 'Enter' ) {
+      this.handleResult()
     }
   }
 
   componentWillMount() {
     document.addEventListener("keydown", this.handleKeydown, false);
   }
-
 
   componentWillUnmount() {
     document.removeEventListener("keydown", this.handleKeydown, false);
@@ -86,16 +92,17 @@ class App extends Component {
   renderEquation() {
     const math = this.state.math;
     const operationMap = {
-      addition: '+',
-      subtraction: '-',
-      multiply: 'x',
-      divide: '/'
-    }
+      addition: "+",
+      subtraction: "-",
+      multiply: "x",
+      divide: "/"
+    };
     return (
       <div>
-        {this.state.prevNumber} {operationMap[math]} {this.state.number}
+        {this.state.prevNumber} {operationMap[math]} {this.state.number}{" "}
+        {this.state.result !== "" && '='} {this.state.result}
       </div>
-    )
+    );
   }
 
   render() {
@@ -103,33 +110,34 @@ class App extends Component {
       <div className="App">
         <input type="text" disabled value={this.state.number} />
         <div>
-          <button onClick={() => this.handleClear()} >C</button>
-          <button onClick={() => this.handleResult()} >=</button>
-          <button onClick={() => this.handleOperator('addition')} >+</button>
-          <button onClick={() => this.handleOperator('subtraction')} >-</button>
+          <button onClick={() => this.handleClear()}>C</button>
+          <button onClick={() => this.handleResult()}>=</button>
+          <button onClick={() => this.handleOperator("addition")}>+</button>
+          <button onClick={() => this.handleOperator("subtraction")}>-</button>
         </div>
         <div>
-          <button onClick={() => this.handleNumber(1)} >1</button>
-          <button onClick={() => this.handleNumber(2)} >2</button>
-          <button onClick={() => this.handleNumber(3)} >3</button>
-          <button onClick={() => this.handleOperator('multiply')} >X</button>
+          <button onClick={() => this.handleNumber(1)}>1</button>
+          <button onClick={() => this.handleNumber(2)}>2</button>
+          <button onClick={() => this.handleNumber(3)}>3</button>
+          <button onClick={() => this.handleOperator("multiply")}>X</button>
         </div>
         <div>
-          <button onClick={() => this.handleNumber(4)} >4</button>
-          <button onClick={() => this.handleNumber(5)} >5</button>
-          <button onClick={() => this.handleNumber(6)} >6</button>
-          <button onClick={() => this.handleOperator('divide')}>/</button>
+          <button onClick={() => this.handleNumber(4)}>4</button>
+          <button onClick={() => this.handleNumber(5)}>5</button>
+          <button onClick={() => this.handleNumber(6)}>6</button>
+          <button onClick={() => this.handleOperator("divide")}>/</button>
         </div>
         <div>
-          <button onClick={() => this.handleNumber(7)} >7</button>
-          <button onClick={() => this.handleNumber(8)} >8</button>
-          <button onClick={() => this.handleNumber(9)} >9</button>
+          <button onClick={() => this.handleNumber(7)}>7</button>
+          <button onClick={() => this.handleNumber(8)}>8</button>
+          <button onClick={() => this.handleNumber(9)}>9</button>
         </div>
         <div>
-          <button onClick={() => this.handleNumber(0)} >0</button>
+          <button onClick={() => this.handleNumber(0)}>0</button>
         </div>
-        <div style={{ height: "1em", borderStyle: "solid"}}>{this.renderEquation()}</div>
-        <div> GitHub Push Test </div>
+        <div style={{ height: "1em", borderStyle: "solid" }}>
+          {this.renderEquation()}
+        </div>
       </div>
     );
   }
